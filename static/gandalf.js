33373facb5936de9fe588fdc88696b2b15d23c12
Evencaster <iloveyoucreator@gmail.com>
2020-12-30 23:04:58 +0300

    gandalf updated

--- static/gandalf.js
+++ static/gandalf.js
@@ -1,10 +1,24 @@
 document.getElementById('flex').width = screen.width
 document.getElementById('flex').height = screen.height
 
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen()
+    } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen()
+    } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen()
+    }
+}
+
 
 document.getElementById('go').onclick = function (event) {
-    document.documentElement.requestFullscreen()
-    document.getElementById('go').style.display = "none"
-    document.getElementById('flex').style.display = "block"
-    document.getElementById('sound').play()
+    // if (window.matchMedia("(min-width: 768px)").matches)
+    openFullscreen(document.documentElement)
+    document.getElementById('go').style.display = 'none'
+    document.getElementById('flex').style.display = 'block'
+
+    setInterval(() => {
+        document.getElementById('sound').play()
+    }, 500)
 }
